cf493413e05046294c766fae4dbd6913f442cade
Variandr <maxim.savchenko.job@gmail.com>
2021-11-15 20:35:55 +0200

    Added navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Header from "./Header/header";
 import Navbar from "./Navbar/navbar";
 import Users from "./Content/Users/users";
 import LoginContainer from "./Login/loginContainer";
 
 const App = (props) => {
-    const isAuth = false;
+    const isAuth = true;
     return (
         <BrowserRouter>
             {isAuth ? (
                     <div className='app'>
-                        <Header className='head'/>
                         <Navbar className='nav'/>
                         <div className='content'>
                             <Routes>
                                 <Route path='/users' render={() => <Users/>}/>
+                                {/*<Route path='/feed' render={() => <Home/>}/>*/}
+                                {/*<Route path='/profile' render={() => <Profile/>}/>*/}
                             </Routes>
                         </div>
                     </div>
--- front/src/Navbar/navbar.jsx
+++ front/src/Navbar/navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import s from './navbar.module.css';
+import {FaUsers, AiFillHome} from "react-icons/all";
 
 const Navbar = (props) => {
     return (
-        <header>
+        <header className={s.nav}>
             <span>
-                <NavLink to='/users'className={s.button}>Users</NavLink>
+                <NavLink to='/feed' className={s.button}><AiFillHome/></NavLink>
+                <NavLink to='/users' className={s.button}><FaUsers/></NavLink>
+            </span>
+            <span>
+                {/*{props.user.photo !== null*/}
+                {/*    ? <img src={props.user.photo}/>*/}
+                {/*    : <></>*/}
+                {/*}*/}
+                <NavLink to='/profile' className={s.profile}>Name</NavLink>
             </span>
         </header>
     )
