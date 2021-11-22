2cf7dab3457a8235e8302c63cc5a48f8eec66e41
Your Name <you@example.com>
2021-11-22 02:52:55 +0200

    added session support, login reg logout requests, systemize response

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const session = require('express-session');
+
 const userRouter = require('./routes/user.route');
+const authRouter = require('./routes/auth.route');
+
 const config = require('./config.json');
+const cors = require('cors');
 
 const PORT = process.env.PORT || config.PORT;
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
+server.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: false,
+    saveUninitialized: true
+}));
 server.use('/api', userRouter);
+server.use('/auth', authRouter);
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT} PORT`);
